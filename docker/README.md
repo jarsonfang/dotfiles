65e862cd63a157e4a9ac9f5307c38c17625580a1
Jarson Fang <huangcai.fang@narwal.com>
2021-05-27 10:37:13 +0800

    update docker utils

--- docker/README.md
+++ docker/README.md
@@ -6,3 +6,11 @@ $ ln -s ~/dotfiles/docker .docker
 $ cd /etc
 $ sudo ln -s ~/dotfiles/docker/etc/docker
 ```
+
+## 解决 docker 系统里面中文显示乱码问题
+
+在 `.bashrc` 文件中添加以下内容
+
+```bash
+export LANG=C.UTF-8
+```
--- docker/enter_ubuntu_docker.sh
+++ docker/enter_ubuntu_docker.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 WORK_DIR=`pwd`
+
 docker_image_tag=20.04.1
+docker_image_repo="ubuntu"
+# docker_image_repo="docker.domain.com:5000/ubuntu"
+
 machine_name=ubuntu-${docker_image_tag}
 machine_id=`docker ps -a -q -f name=${machine_name}`
 
@@ -14,7 +18,8 @@ if [[ ${machine_id} != '' ]]; then
     docker start ${machine_id}
     docker exec -it ${machine_id} /bin/bash
 else
-    docker_addr=ubuntu:${docker_image_tag}
+    docker_addr=${docker_image_repo}:${docker_image_tag}
+    # docker pull $docker_addr
 
     log_highlight "Run docker from ${docker_addr}"
 
