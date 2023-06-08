ed21a92547dcd3ce49c5daa4950dcb3407dd6a88
fanghc2 <fanghc2@sany.com.cn>
2023-06-08 17:53:10 +0800

    update enter docker scripts
    Signed-off-by: fanghc2 <fanghc2@sany.com.cn>

--- docker/enter_x86_docker.sh
+++ docker/enter_x86_docker.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-# Preparation:
-# download qemu-aarch64-static from https://github.com/multiarch/qemu-user-static/releases
-# copy qemu-aarch64-static to /usr/bin (give execute permission)
-#
-# docker pull --platform=linux/aarch64 ubuntu:18.04
-# docker pull multiarch/qemu-user-static:register
-
-docker_image_tag="18.04"
+docker_image_tag="20.04"
 docker_image_repo="ubuntu"
 
-machine_name=ubuntu-aarch64-${docker_image_tag}
+machine_name=ubuntu-${docker_image_tag}
 machine_id=`docker ps -a -q -f name=${machine_name}`
 
 log_highlight() {
@@ -20,23 +13,20 @@ log_highlight() {
 # sudo apt install x11-xserver-utils
 xhost +
 
-docker run --rm --privileged multiarch/qemu-user-static:register --reset
-
 if [[ ${machine_id} != '' ]]; then
     log_highlight "Enter docker: ${machine_name}/${machine_id} ..."
     docker start ${machine_id}
-    docker exec -itu root ${machine_id} /bin/bash
+    docker exec -it ${machine_id} /bin/bash
 else
     WORK_DIR=`pwd`
     docker_image_url=${docker_image_repo}:${docker_image_tag}
 
     log_highlight "Run docker from ${docker_image_url}"
 
-    docker run -it --privileged --name ${machine_name} --net=host \
+    docker run --pull=missing -it --privileged --name ${machine_name} --net=host \
         -v /dev:/dev \
         -v ${WORK_DIR}:/workspace -w /workspace \
         -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY \
-        -v /usr/bin/qemu-aarch64-static:/usr/bin/qemu-aarch64-static \
         ${docker_image_url}
 fi
 
