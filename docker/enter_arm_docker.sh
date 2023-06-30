309c9628d78735fcc016fb39f0928969942906f6
fanghc2 <fanghc2@sany.com.cn>
2023-06-30 09:18:06 +0800

    update enter docker scripts
    Signed-off-by: fanghc2 <fanghc2@sany.com.cn>

--- docker/enter_arm_docker.sh
+++ docker/enter_arm_docker.sh
@@ -5,34 +5,34 @@
 # copy qemu-aarch64-static to /usr/bin (give execute permission)
 #
 # docker pull --platform=linux/aarch64 ubuntu:18.04
-# docker pull multiarch/qemu-user-static:register
 
 docker_image_tag="18.04"
 docker_image_repo="ubuntu"
+docker_image_url=${docker_image_repo}:${docker_image_tag}
 
 machine_name=ubuntu-aarch64-${docker_image_tag}
 machine_id=`docker ps -a -q -f name=${machine_name}`
 
-log_highlight() {
-    echo -e "\e[30;31m"$1"$(tput sgr0)"
+function log_highlight()
+{
+    echo -e "\e[30;31m"$1"\e[0m"
 }
 
 # sudo apt install x11-xserver-utils
 xhost +
 
-docker run --rm --privileged multiarch/qemu-user-static:register --reset
+docker run --pull=missing --rm --privileged multiarch/qemu-user-static:register --reset
 
-if [[ ${machine_id} != '' ]]; then
+if [[ x${machine_id} != x'' ]]; then
     log_highlight "Enter docker: ${machine_name}/${machine_id} ..."
     docker start ${machine_id}
-    docker exec -itu root ${machine_id} /bin/bash
+    docker exec -it ${machine_id} /bin/bash
 else
-    WORK_DIR=`pwd`
-    docker_image_url=${docker_image_repo}:${docker_image_tag}
-
     log_highlight "Run docker from ${docker_image_url}"
 
-    docker run -it --privileged --name ${machine_name} --net=host \
+    WORK_DIR=${PWD}
+    docker run -it --privileged --name ${machine_name} \
+        -u root --net=host \
         -v /dev:/dev \
         -v ${WORK_DIR}:/workspace -w /workspace \
         -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY \
--- docker/enter_x86_docker.sh
+++ docker/enter_x86_docker.sh
@@ -2,28 +2,29 @@
 
 docker_image_tag="20.04"
 docker_image_repo="ubuntu"
+docker_image_url=${docker_image_repo}:${docker_image_tag}
 
 machine_name=ubuntu-${docker_image_tag}
 machine_id=`docker ps -a -q -f name=${machine_name}`
 
-log_highlight() {
-    echo -e "\e[30;31m"$1"$(tput sgr0)"
+function log_highlight()
+{
+    echo -e "\e[30;31m"$1"\e[0m"
 }
 
 # sudo apt install x11-xserver-utils
 xhost +
 
-if [[ ${machine_id} != '' ]]; then
+if [[ x${machine_id} != x'' ]]; then
     log_highlight "Enter docker: ${machine_name}/${machine_id} ..."
     docker start ${machine_id}
     docker exec -it ${machine_id} /bin/bash
 else
-    WORK_DIR=`pwd`
-    docker_image_url=${docker_image_repo}:${docker_image_tag}
-
     log_highlight "Run docker from ${docker_image_url}"
 
-    docker run --pull=missing -it --privileged --name ${machine_name} --net=host \
+    WORK_DIR=${PWD}
+    docker run --pull=missing -it --privileged --name ${machine_name} \
+        -u root --net=host \
         -v /dev:/dev \
         -v ${WORK_DIR}:/workspace -w /workspace \
         -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY \
